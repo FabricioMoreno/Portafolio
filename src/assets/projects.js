0ee078ced4d48d8309c01f8ce21443f1bcb637b8
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2022-12-30 06:04:55 -0500

    Added some projects like cards in portafolio

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,37 +1,64 @@
+//Images projects
 import cesarEncryption from "../images/projects/cesarEncryption.png"
+import secret from "../images/projects/secret.png"
+import retos from "../images/projects/retos.png"
+import qrCode from "../images/projects/qrCode.png"
+import toDoList from "../images/projects/toDoList.png"
+import forx from "../images/projects/forx.png"
+//import  from "../images/projects/cesarEncryption.png"
+//import  from "../images/projects/cesarEncryption.png"
+//import  from "../images/projects/cesarEncryption.png"
+//import  from "../images/projects/cesarEncryption.png"
+
 
 const projects = [
     {
         id:1,
         title: "Cesar's Encryption",
-        description:"sdfhjsdgfjh",
+        description:"Proyecto basado en el famoso y antiguo método de encriptación César. Elige tus llaves para codificar y decodificar el mensaje.",
         linkGithub:"https://github.com/FabricioMoreno/Cesar-s-Encryption",
         linkWebsite:"https://fabriciomoreno.github.io/Cesar-s-Encryption/",
         img:cesarEncryption
     },
     {
         id:2,
-        title: "Cesar's Encryption",
-        description:"sdfhjsdgfjh",
-        linkGithub:"https://github.com/FabricioMoreno/Cesar-s-Encryption",
-        linkWebsite:"https://fabriciomoreno.github.io/Cesar-s-Encryption/",
-        img:cesarEncryption
+        title: "Secret",
+        description:"No hay secreto que no pueda ser bien guardado, agrega una capa de seguridad a tus mensajes.",
+        linkGithub:"https://github.com/FabricioMoreno/secret",
+        linkWebsite:"https://fabriciomoreno.github.io/secret/index.html",
+        img:secret
     },
     {
         id:3,
-        title: "Cesar's Encryption",
-        description:"sdfhjsdgfjh",
-        linkGithub:"https://github.com/FabricioMoreno/Cesar-s-Encryption",
-        linkWebsite:"https://fabriciomoreno.github.io/Cesar-s-Encryption/",
-        img:cesarEncryption
+        title: "Retos",
+        description:"Diviértete con el azar de los retos, el que te toque tienes que hacerlo.",
+        linkGithub:"https://github.com/FabricioMoreno/retos",
+        linkWebsite:"https://fabriciomoreno.github.io/retos/index.html",
+        img:retos
     },
     {
         id:4,
-        title: "Cesar's Encryption",
-        description:"sdfhjsdgfjh",
-        linkGithub:"https://github.com/FabricioMoreno/Cesar-s-Encryption",
-        linkWebsite:"https://fabriciomoreno.github.io/Cesar-s-Encryption/",
-        img:cesarEncryption
+        title: "QrCode",
+        description:"Haz tus propios códigos qr, convierte cualquier texto en ello.",
+        linkGithub:"https://github.com/FabricioMoreno/QrCode",
+        linkWebsite:"https://fabriciomoreno.github.io/QrCode/",
+        img: qrCode
+    },
+    {
+        id:5,
+        title: "ToDoList",
+        description:"Divide la planificación de tus actividades en pequeños objetivos y vencerás.",
+        linkGithub:"https://github.com/FabricioMoreno/ToDoList",
+        linkWebsite:"https://fabriciomoreno.github.io/ToDoList/",
+        img: toDoList
+    },
+    {
+        id:6,
+        title: "Forx",
+        description:"Juego basado en el libro del hombre que calculaba de Malba Tahan, pruébalo ya.",
+        linkGithub:"https://github.com/FabricioMoreno/Forx",
+        linkWebsite:"https://fabriciomoreno.github.io/Forx/",
+        img: forx
     }
 ]
 
