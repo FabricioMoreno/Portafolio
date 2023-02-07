c52da9913195112b7bff4b419dfa0cf4d6ec8920
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-02-07 09:58:39 -0500

    Added a js object to list all links to contact

---
+++ src/assets/contactsLinks.js
@@ -0,0 +1,23 @@
+import linkedin from "../images/icons/linkedin.svg"
+import email from "../images/icons/email.svg"
+import github from "../images/icons/github.svg"
+
+const contactsLinks = [
+    {
+        name: "LinkedIn",
+        url:"https://www.linkedin.com/in/fabriciomorenonaranjo",
+        img: linkedin
+    },
+    {
+        name:"Email",
+        url:"fabricio.morenonaranjo@hotmail.com",
+        img: email
+    },
+    {
+        name:"GitHub",
+        url:"https://github.com/FabricioMoreno",
+        img:github
+    }
+]
+
+export default contactsLinks
